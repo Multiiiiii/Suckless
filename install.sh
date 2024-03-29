2dde79c461ae62d10381bd610929617dba5a8882
m <multimitamiensaft@gmail.com>
2024-03-29 19:03:35 +0100

    made ☣ great again

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-pacman -S --needed - $(comm -12 <(pacman -Slq | sort) <(sort packages.txt))
+sudo pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort packages.txt))
 cd dwm && sudo make clean install
 cd ..
 cd slstatus && sudo make clean install
@@ -6,8 +6,8 @@ cd ..
 cd st && sudo make clean install
 mv .xinitrc ~/ 
 mv .bashrc ~/ 
-mv setbg /usr/bin/ 
-mv default.jsonc /usr/share/fastfetch/presets/
+sudo mv setbg /usr/bin/ 
+sudo mv default.jsonc /usr/share/fastfetch/presets/
 systemctl enable ufw
 git clone https://aur.archlinux.org/paru-bin.git
 cd paru-bin && makepkg -sri
--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack Nerd Font Mono:style=Regular:pixelsize=15:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=15:antialias=true:autohint=true";
 static char *font2[] = { "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 2;
 
