becdbb48a17682e83520f7d3fb48f325a944fcca
m <multimitamiensaft@gmail.com>
2024-03-29 15:12:23 +0100

    added to install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
-pacman -S --needed - < packages.txt
+pacman -S --needed - $(comm -12 <(pacman -Slq | sort) <(sort packages.txt))
+cd dwm && sudo make clean install
+cd ..
+cd slstatus && sudo make clean install
+cd ..
+cd st && sudo make clean install
 mv .xinitrc ~/ 
 mv .bashrc ~/ 
 mv setbg /usr/bin/ 
 mv default.jsonc /usr/share/fastfetch/presets/
+systemctl enable ufw
+git clone https://aur.archlinux.org/paru-bin.git
+cd paru-bin && makepkg -sri
 git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
