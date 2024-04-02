9c7b25e3a04c8fb6599598b38628ec465aff29ad
m <multimitamiensaft@gmail.com>
2024-04-02 15:11:22 +0200

    edited README and install.sh

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 My Suckless stuff
+The install.sh applies the neo keyboard Layout globaly. To stop that just remove the lines with 00-Keboard.conf and vconsole.conf. Or just delete the files.
--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
+git clone https://aur.archlinux.org/paru-bin.git
+cd paru-bin && makepkg -sri
+cd ~
 sudo pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort packages.txt))
 cd dwm && sudo make clean install
 cd ..
 cd slstatus && sudo make clean install
 cd ..
 cd st && sudo make clean install
+mkdir Pictures
+mkdir Vids
+cd Pictures
+mkdir Wall
+cd .. 
 mv .xinitrc ~/ 
 mv .bashrc ~/ 
 sudo mv 00-keyboard.conf /etc/X11/xorg.conf.d/
@@ -11,6 +19,4 @@ sudo mv vconsole.conf /etc/
 sudo mv setbg /usr/bin/ 
 sudo mv default.jsonc /usr/share/fastfetch/presets/
 systemctl enable ufw
-git clone https://aur.archlinux.org/paru-bin.git
-cd paru-bin && makepkg -sri
 git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
