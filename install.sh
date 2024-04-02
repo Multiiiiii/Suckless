ced8c3c0a606a2f5f168eb23d232d0aa8f02e77f
m <multimitamiensaft@gmail.com>
2024-04-02 10:58:42 +0200

    added install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ cd ..
 cd st && sudo make clean install
 mv .xinitrc ~/ 
 mv .bashrc ~/ 
+sudo mv 00-keyboard.conf /etc/X11/xorg.conf.d/
 sudo mv setbg /usr/bin/ 
 sudo mv default.jsonc /usr/share/fastfetch/presets/
 systemctl enable ufw
 git clone https://aur.archlinux.org/paru-bin.git
 cd paru-bin && makepkg -sri
-setxkbmap -layout de neo
 git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
