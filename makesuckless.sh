38415f86da237ec2cb2440b01ea486a38885477e
Multiiiiii <149270406+Multiiiiii@users.noreply.github.com>
2024-04-03 17:54:30 +0000

    make dmenu makesuckless.sh

--- makesuckless.sh
+++ makesuckless.sh
@@ -4,6 +4,9 @@ cd ..
 echo "making slstatus"
 cd slstatus && sudo make clean install
 cd ..
+echo "making dmenu"
+cd dmenu && sudo make clean install
+cd ..
 echo "making st"
 cd st && sudo make clean install
 cd ..
