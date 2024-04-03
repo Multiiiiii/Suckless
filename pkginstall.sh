618e595d74a1e26f64a5ee93e432a263b16caae5
Multiiiiii <149270406+Multiiiiii@users.noreply.github.com>
2024-04-03 17:35:39 +0000

    --needed to  pkginstall.sh

--- pkginstall.sh
+++ pkginstall.sh
@@ -3,7 +3,7 @@
 # Read the package list
 while IFS= read -r package; do
     # Check if the package is available in the official repositories
-    if pacman -Si "$package" >/dev/null 2>&1; then
+    if pacman -Si --needed "$package" >/dev/null 2>&1; then
         echo "Installing $package with pacman..."
         sudo pacman -S --noconfirm "$package"
     else
