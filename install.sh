906b6453e18b0b60485e74a7df39dc2fe37a26ba
Multiiiiii <149270406+Multiiiiii@users.noreply.github.com>
2024-04-03 18:22:28 +0000

    merged sh files

--- install.sh
+++ install.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 echo "installing Paru"
+./getparu.sh
 echo "installing nesesary packages"
+./pkginstall.sh
 echo "enabling firewall"
+./firewall.sh
 echo "chreating directorys for rice and yt-dlp"
+./mkingdir.sh
+echo "making Suckless Stuff"
+./makesuckless.sh
 echo "moving config dotfiles"
+./dotfiles.sh
 echo "moving keyboard Layout files"
+./neoboard.sh
 echo "moving setbg"
+./backg.sh
 echo "moving fastfetch preset"
+./fast.sh
 echo "making NvChad"
+./nvchad.sh
 
 
