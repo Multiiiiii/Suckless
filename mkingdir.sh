c7397eea3b5d50fd3621b3f9fe29541bf2d7b671
m <multimitamiensaft@gmail.com>
2024-04-19 21:35:12 +0200

    changed default Wallpapers

--- mkingdir.sh
+++ mkingdir.sh
@@ -4,5 +4,6 @@ mkdir Pictures
 mkdir Vids
 cd Pictures
 mkdir Wall
-mv ~/Suckless/black-hole-wallpaper.webp ~/Pictures/Wall
+mv ~/Suckless/dark-theme-1080.png ~/Pictures/Wall
+mv ~/Suckless/light-theme-1080.png ~/Pictures/Wall
 cd ..
