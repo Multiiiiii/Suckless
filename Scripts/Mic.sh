c33fcb2d8e7d4e7c03669aac6032b06a9d2be972
Multiiiiii <multimitamiensaft@gmail.com>
2025-08-04 19:42:20 +0200

    rm =

--- Scripts/Mic.sh
+++ Scripts/Mic.sh
@@ -5,7 +5,7 @@
 MUTE_STATUS=$(pactl list sources | grep -A 10 'input' | grep 'Mute:' | awk '{print $2}')
 
 # Check if the mute status is 'yes' or 'no'
-if [ "$MUTE_STATUS" == "yes" ]; then
+if [ "$MUTE_STATUS" = "yes" ]; then
     # If muted, output the Unicode symbol for muted ()
     echo " "
 else
