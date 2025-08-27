41462148d234e2d260ef477dffadb584a49a7bdc
Multiiiiii <149270406+Multiiiiii@users.noreply.github.com>
2025-08-27 20:19:39 +0200

    Replace mv with cp for udiskie-dmenu
    Change mv to cp for udiskie-dmenu installation.

--- debpkginstall.sh
+++ debpkginstall.sh
@@ -97,7 +97,7 @@ cd i3lock-color
 ./install-i3lock-color.sh
 cd suckless
 wget https://raw.githubusercontent.com/betterlockscreen/betterlockscreen/main/install.sh -O - -q | sudo bash -s system
-mv debstuff/udiskie-dmenu /usr/bin/
+cp debstuff/udiskie-dmenu /usr/bin/
 pipx install pywal
 sudo cp ~/.local/bin/wal /usr/bin/
 git clone https://github.com/cdown/clipmenu.git
