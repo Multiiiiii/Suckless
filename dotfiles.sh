0b4c0ddf7459af0e0bd057bb595ba8348eed27eb
Multiiiiii <149270406+Multiiiiii@users.noreply.github.com>
2025-09-03 13:07:10 +0200

    updated /home/m/Suckless

--- dotfiles.sh
+++ dotfiles.sh
@@ -3,7 +3,7 @@ cp .xinitrc ~/
 cp .bashrc ~/ 
 cp vifmrc ~/.config/vifm/vifmrc
 cp vifmrun ~/.config/vifm/scripts/vifmrun
+cp vifmimg ~/.config/vifm/scripts/vifmrun
 cp palenight.vifm ~/.config/vifm/colors/
 cp 00-touchpad.conf /etc/X11/xorg.conf.d/
 cp -r qutebrowser ~/.config/
-
